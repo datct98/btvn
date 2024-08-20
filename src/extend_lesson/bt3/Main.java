c1f54e6aecb38b0f025273da9d4e69251cab1251
datct15 <kiwisun1998@gmail.com>
2024-08-20 21:10:27 +0700

    Chữa ví dụ bài 3

---
+++ src/extend_lesson/bt3/Main.java
@@ -0,0 +1,15 @@
+package extend_lesson.bt3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // type arrName [] = new [number]
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number of employees?");
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Employee employees [] = new Employee[n];
+        // BTVN làm nốt
+    }
+}
