e15b81a6b2605bc3ebac58cf2a3ec0096d7ea365
datct15 <kiwisun1998@gmail.com>
2024-08-20 19:19:37 +0700

    BT tách hàm

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,49 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Hãy nhập giá trị a: ");
-        double a = Double.parseDouble(scanner.nextLine());
-        System.out.println("Hãy nhập giá trị b: ");
-        double b = Double.parseDouble(scanner.nextLine());
-        System.out.println("Hãy nhập giá trị c: ");
-        double c = Double.parseDouble(scanner.nextLine());
+        // Khởi tạo đt -> Dùng hàm khởi tạo
+        Account accountSystem = new Account("techmaster", "hoclacoviec", 1000000);
 
-        if(a !=0){
-            double denta = Math.pow(b, 2) - 4*a*c;
-            if(denta< 0){
-                System.out.println("PT vn");
-            } else if (denta == 0 ){
-                double x = -b/(2*a);
+        do {
+            boolean isLoginSuccess = login(scanner, accountSystem);
+            if(!isLoginSuccess){
+                System.out.println("Thất bịa rồi");
             } else {
-                double x1 = (-b + Math.sqrt(denta)) /(2*a);
-                double x2 = (-b - Math.sqrt(denta)) /(2*a);
-            }
-        } else {
-            System.out.println("Giải pt b1");
-        }
+                System.out.println("Login thành công, mời bạn lựa chọn");
+                do {
+                    System.out.println("1- Xem thông tin tài khoản");
+                    System.out.println("2- Rút tiền");
+                    int choice = Integer.parseInt(scanner.nextLine());
+                    switch (choice){
+                        case 1:
+                            System.out.println(accountSystem);
+                            break;
+                        case 2:
+                            System.out.println("Mời b nhập số tiền muốn rút");
+                            double money = Double.parseDouble(scanner.nextLine());
+                            if(money<=0 || money> accountSystem.getBalance()){
+                                System.out.println("Số tiền k hợp lệ");
+                            } else {
+                                double remainAmt = accountSystem.getBalance()- money;
+                                accountSystem.setBalance(remainAmt);
+                            }
+                            break;
+                    }
+                } while (true);
 
+            }
+
+        } while (true);
+    }
+
+    public static boolean login(Scanner scanner, Account accountSystem){
+        System.out.println("Mời b nhập username: ");
+        String username = scanner.nextLine();
+        System.out.println("Mời b nhập pass: ");
+        String pass = scanner.nextLine();
+        if(username.equals(accountSystem.getUsername()) && pass.equals(accountSystem.getPassword())){
+           return true;
+        }
+        return false;
     }
 }
